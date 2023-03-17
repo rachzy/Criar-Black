f1ca808af888e49a7eaa0951cdada6b1c3e08f36
Henrique <henriqueferreira.fox@gmail.com>
2023-03-17 19:47:51 -0300

    adicionar tilt

--- src/components/ServiceBox/index.tsx
+++ src/components/ServiceBox/index.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 import "./ServiceBox.css";
 import "./ServiceBox-Mobile.css";
 
 interface IProps {
-    titulo: string;
-    thumbnail: string;
-    desc: string;
+  titulo: string;
+  thumbnail: string;
+  desc: string;
 }
 
-const ServiceBox: React.FC<IProps> = ({titulo, thumbnail, desc}) => {
+const ServiceBox: React.FC<IProps> = ({ titulo, thumbnail, desc }) => {
   return (
-    <div className="service-box">
-    <h2>{titulo}</h2>
-    <div className="service-img">
-      <img src={thumbnail} alt={titulo} />
+    <div className="service-box" data-tilt>
+      <h2>{titulo}</h2>
+      <div className="service-img">
+        <img src={thumbnail} alt={titulo} />
+      </div>
+
+      <h3>{desc}</h3>
     </div>
+  );
+};
 
-    <h3>{desc}</h3>
-  </div>
-  )
-}
-
-export default ServiceBox
+export default ServiceBox;
